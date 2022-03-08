dc29c38f1053a34dbc029cde55a2b3ff5d237764
aidonmccomb <101207078+aidonmccomb@users.noreply.github.com>
2022-03-08 12:49:20 -0500

    base project

---
+++ test_app/test_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test_app
+//
+//  Created by Student on 2022-03-08.
+//  Copyright © 2022 Student. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
