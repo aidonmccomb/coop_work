527e98ccaee3a814dd10a8a2d3d96513f5b09a7a
aidonmccomb <101207078+aidonmccomb@users.noreply.github.com>
2022-03-10 14:25:35 -0500

    Dice class added

--- test_app/test_app/ContentView.swift
+++ test_app/test_app/ContentView.swift
@@ -19,3 +19,26 @@ struct ContentView_Previews: PreviewProvider {
         ContentView().previewLayout(.sizeThatFits)
     }
 }
+
+//comment
+
+import UIKit
+
+class Die {
+    var faceValue : Int?
+    
+    init () {
+        self.faceValue = nil
+    }
+    
+    func rollDie(){
+        let num = Int.random(in:1...6)
+        faceValue = num
+    }
+}
+
+var Dice = Die()
+
+//Dice.rollDie()
+
+//print("\(String(describing: Dice.faceValue!))")
