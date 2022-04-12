4de07cdcaa359377a3e699e4886be6036c85c651
aidonmccomb <101207078+aidonmccomb@users.noreply.github.com>
2022-04-11 23:13:25 -0400

    work
    work

--- MyWorkspace/2-Algorithms/3-Build a QuestionBot App/QuestionAnswerer.playground/Pages/Special Questions.xcplaygroundpage/Contents.swift
+++ MyWorkspace/2-Algorithms/3-Build a QuestionBot App/QuestionAnswerer.playground/Pages/Special Questions.xcplaygroundpage/Contents.swift
@@ -1,5 +1,5 @@
 /*:
-## Special Questions
+ ## Special Questions
  As well as having general answers to who, what, where questions, you can include some special answers to specific questions.
  */
 func responseTo(question: String) -> String {
@@ -8,16 +8,19 @@ func responseTo(question: String) -> String {
     print(lowerQuestion)
     
     if lowerQuestion.hasPrefix("where") {
-        return "To the North!"
-    } else if lowerQuestion == "where are the cookies?" {
-        return "In the cookie jar!"
-    } else {
-        return "That really depends"
+        if lowerQuestion == "where are the cookies?" {
+            return "In the cookie jar!"
+        } else {
+            return "To the North!"
+        } } else {
+            return "That really depends"
+        }
     }
-}
-responseTo(question: "Where are the cookies?")
+
+var x = responseTo(question: "Where are the cookies?")
+print(x)
 /*:
  - callout(Exercise): The function above doesn’t work. The first `if` statement asks if the question starts with “where,” which it does. This means the later statements are never tested. Change the function above so that you get the answer “In the cookie jar!” to the question “Where are the cookies?”
-
-[Previous](@previous)  |  page 4 of 7  |  [Next: Default Answers](@next)
+ 
+ [Previous](@previous)  |  page 4 of 7  |  [Next: Default Answers](@next)
  */
